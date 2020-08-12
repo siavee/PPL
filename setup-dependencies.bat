be58a34b0d595acaf2782d4db2d677abca03c2cc
Muhammad Taufik Tirtosudiro <muhtaufikt@gmail.com>
2020-08-12 11:35:59 +0700

    windows dependency script setup will not attempt to setup vcpkg if it already exists

--- setup-dependencies.bat
+++ setup-dependencies.bat
@@ -1,5 +1,9 @@
-git clone https://github.com/Microsoft/vcpkg
-call .\vcpkg\bootstrap-vcpkg.bat
+if not exist .\vcpkg (
+	git clone https://github.com/Microsoft/vcpkg
+)
+if not exist .\vcpkg\vcpkg.exe (
+	call .\vcpkg\bootstrap-vcpkg.bat
+)
 
 .\vcpkg\vcpkg.exe install freetype:x64-windows-static
 .\vcpkg\vcpkg.exe install glew:x64-windows-static
